658ee126c6612021b2fbe93b9079fa3ba055558f
นางสาวทิชาพร ลีดี <59079035+tichaporn@users.noreply.github.com>
2019-12-27 22:37:26 +0700

    oop

---
+++ src/Main/Student.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class Student {
+    //ประกาศตัวแปรของ class
+    public String name;
+    public String id;
+    public String major;
+}
---
+++ src/Main/main.java
@@ -0,0 +1,35 @@
+package Main;
+
+import javax.swing.*;
+
+public class main {
+
+    public static void Main(String[] args) {
+        //create Student object
+        Student tichaporn = new Student();
+
+        //input data to Object
+        tichaporn.name = "Tichaporn Leedee";
+        tichaporn.id = "623410005-4";
+        tichaporn.major = "CIS";
+
+        //get data from Object
+        System.out.println("1. " +tichaporn.name + " " + tichaporn.major)
+
+        //add student to array
+        //array index
+        Student[] oopStudent = {tichaporn};
+
+        System.out.println("1. " + oopStudent[0].name);
+
+        find_CIS_Student(oopStudent);
+    }
+    public void find_CIS_Student(Student[] students){
+        for(int i=0;i<students.length;i++){
+            //check student.major is "CIS"
+            if(students[i].major=="CIS"){
+                System.out.println(i+1+". "+students[i].name);
+            }
+        }
+    }
+}
